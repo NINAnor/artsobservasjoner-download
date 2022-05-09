dedbb8ddd019c2b4df14b6d34af8e417d8aabd57
Francesco Frassinelli <francesco.frassinelli@nina.no>
2022-05-09 12:57:15 +0200

    Add code

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # artsobservasjoner-download
+
+## Dependencies
+
+- Python
+  - requests
+  - tqdm
+
+## Configuration
+
+1. Open `artsobservasjoner.py`
+2. Change `areas`
+3. Add or remove fields in `fields`
+4. Write species in `species.txt` (one per line)
+
+## Run
+
+Execute `artsobservasjoner.py`.
+Open `output.csv`.
---
+++ artsobservasjoner.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+import requests
+import tqdm
+
+API = "https://artskart.artsdatabanken.no/publicapi/"
+areas = "5001"
+fields = [
+    "Id",
+    "ScientificName",
+    "TaxonId",
+    "Sex",
+    "Status",
+    "Count",
+    "Locality",
+    "Habitat",
+    "Latitude",
+    "Longitude",
+    "Projection",
+    "Institution",
+    "Collector",
+    "CollectedDate",
+]
+
+terms = []
+with open("species.txt") as species:
+    for line in species.readlines():
+        terms.append(line.strip())
+
+def get_taxon_from_scientificname(term):
+    taxons = requests.get(
+        f"{API}/api/taxon/short",
+        params={"term": term}
+    ).json()
+    for result in taxons:
+        if result["ScientificName"] == term:
+            return str(result["IntId"])
+
+def get_observations_from_taxon(taxons):
+    params={
+        "Taxons": taxons,
+        "Areas": areas,
+        "pageSize": 1000,
+    }
+    results = requests.get(
+        f"{API}/api/observations/list",
+        params=params,
+    ).json()
+    yield from results["Observations"]
+    total = results["TotalPages"]
+    for index in tqdm.tqdm(range(1, total), desc="Pages", leave=False):
+        params["pageIndex"] = index
+        results = requests.get(
+            f"{API}/api/observations/list",
+            params=params,
+        ).json()
+        yield from results["Observations"]
+
+with open("output.csv", "w") as output:
+    output.write("\t".join(fields))
+    output.write("\n")
+    ids = [get_taxon_from_scientificname(term) for term in terms]
+    taxons = ",".join(ids)
+    observations = get_observations_from_taxon(taxons)
+    for observation in observations:
+        output.write("\t".join(str(observation[field]) for field in fields))
+        output.write("\n")
